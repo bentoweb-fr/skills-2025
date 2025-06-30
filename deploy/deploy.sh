a39b1900bba856832f2fa212dc9165faec54fa78
Benoit Cyrulik <benoit@bentoweb.fr>
2025-06-30 08:53:39 +0200

    test

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -3,13 +3,13 @@ set -e
 
 echo "📦 Déploiement en cours sur $(hostname)..."
 
-cd /home/tonuser/skills2025
+cd /home/benoit/skills2025
 
 # Pull depuis Git (optionnel si tu rsync depuis GitHub)
 # git pull origin main
 
 # Build images (si localement sur le serveur)
-docker compose -f docker-compose.prod.yml down
-docker compose -f docker-compose.prod.yml up -d --build
+docker compose -f docker-compose.prod.yaml down
+docker compose -f docker-compose.prod.yaml up -d --build
 
 echo "✅ Déploiement terminé."
