eb9f2c622ab2ac63fd780470a14ebd659c24100e
bentoweb-fr <benoit@bentoweb.fr>
2025-07-31 17:21:14 +0200

    phpmyadmin #13

--- docker/phpmyadmin/entrypoint.sh
+++ docker/phpmyadmin/entrypoint.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 set -e
 
+# Créer le fichier de configuration secret manquant
+mkdir -p /etc/phpmyadmin
+if [ ! -f /etc/phpmyadmin/config.secret.inc.php ]; then
+    echo "<?php" > /etc/phpmyadmin/config.secret.inc.php
+    echo "\$cfg['blowfish_secret'] = '$(openssl rand -base64 32)';" >> /etc/phpmyadmin/config.secret.inc.php
+fi
+
 # Activer la configuration servername
 a2enconf servername
 
