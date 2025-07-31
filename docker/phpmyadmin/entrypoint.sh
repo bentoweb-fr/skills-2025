22347821d1103ee65d0fd148a2d0e3369128741b
bentoweb-fr <benoit@bentoweb.fr>
2025-07-31 16:26:01 +0200

    phpmyadmin #9

--- docker/phpmyadmin/entrypoint.sh
+++ docker/phpmyadmin/entrypoint.sh
@@ -4,5 +4,8 @@ set -e
 # Ajouter ServerName dans la configuration Apache
 echo 'ServerName api.bentoweb.fr' >> /etc/apache2/apache2.conf
 
+# Tester la configuration Apache
+apache2ctl configtest
+
 # Démarrer Apache
 exec apache2-foreground
